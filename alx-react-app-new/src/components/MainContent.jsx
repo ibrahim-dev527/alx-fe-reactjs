2f201ccbdc5409e2dd304fce6dfeec081972d055
Ibrahim Mohammed Lotsu <lotsuibrahim2@gmail.com>
2026-01-07 00:21:24 -0800

    Added inline stylings

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: '#f0f0f0', padding: '15px', margin: '10px', borderRadius: '5px' }}>
+      <p style={{ color: '#333', fontSize: '16px' }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
