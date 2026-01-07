7d3c986cc1c24943d69b805ad23884341802749d
Ibrahim Mohammed Lotsu <lotsuibrahim2@gmail.com>
2026-01-07 02:03:18 -0800

    Corrected counter.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/counter'
 
 
 function App() {
@@ -26,6 +27,9 @@ function App() {
         age="25"
         bio="Loves hiking and photography"
       />
+    </div>
+     <div>
+      <Counter />
     </div>
       <div>
         <a href="https://vite.dev" target="_blank">
